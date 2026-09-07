50b4bbd006ee25a3ccca2b2f6c5dcc3ae04e9358
dblu13 <dario_bernasconi@icloud.com>
2026-09-07 22:37:15 +0200

    Update segment.sh

--- segment.sh
+++ segment.sh
@@ -18,7 +18,7 @@
 # min_size: Minimum object size in pixel. Smaller segmentations are discarded.
 # max_size_fraction: Maximum obect size as a fraction of the image size. Bigger objects are discarded.
 # cpsam_model: Name of the CPSAM (or path to a custom) model to use.
-# mask_str: Suffix and file extension of the mask. File extension should be .tif, .npy or .npz.
+# mask_str: Suffix that differentiates the mask from its corresponding image (e.g., `_mask.tif`). File extension should be `.tif`, `.npy`, or `.npz`.
 # plot_range: Percentiles defining the data range for the QC plots.
 # pixi_dir: Directory of the pixi workspace
 
